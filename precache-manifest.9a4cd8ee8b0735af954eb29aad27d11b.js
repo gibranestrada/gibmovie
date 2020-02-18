eff03e50fd98b55201600e80f0caf8ffdb0765b9
Gibran Estrada <gibran9@gmail.com>
2020-02-18 14:50:10 -0500

    Updates

---
+++ precache-manifest.9a4cd8ee8b0735af954eb29aad27d11b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147ed1dec3d2ea4b1a0d4a809423ee86",
+    "url": "/gibmovie/index.html"
+  },
+  {
+    "revision": "7ea351ba1871d638f074",
+    "url": "/gibmovie/static/css/2.510ddb82.chunk.css"
+  },
+  {
+    "revision": "0fbc20463e8b27ef3164",
+    "url": "/gibmovie/static/css/main.4b5c55ff.chunk.css"
+  },
+  {
+    "revision": "7ea351ba1871d638f074",
+    "url": "/gibmovie/static/js/2.c0741a3f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/gibmovie/static/js/2.c0741a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbc20463e8b27ef3164",
+    "url": "/gibmovie/static/js/main.ece96ffd.chunk.js"
+  },
+  {
+    "revision": "30092c3e89509fa84c72",
+    "url": "/gibmovie/static/js/runtime-main.ad2568b6.js"
+  }
+]);
