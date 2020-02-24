5b08fa7595fb16e95dbc5a4a71b0491c5fac94a0
Gibran Estrada <gibran9@gmail.com>
2020-02-24 12:25:21 -0500

    Updates

---
+++ precache-manifest.bf2fb67363de80f90c6c9a73d1c1149a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e759ad0268c335c85ce9d4238ef6d9b",
+    "url": "/gibmovie/index.html"
+  },
+  {
+    "revision": "6f2cb7f731f1666d1c7d",
+    "url": "/gibmovie/static/css/2.510ddb82.chunk.css"
+  },
+  {
+    "revision": "d89d6122c38a467f143e",
+    "url": "/gibmovie/static/css/main.adb74753.chunk.css"
+  },
+  {
+    "revision": "6f2cb7f731f1666d1c7d",
+    "url": "/gibmovie/static/js/2.efd291a8.chunk.js"
+  },
+  {
+    "revision": "74b5b99a348fedd13ab1cb018bec8748",
+    "url": "/gibmovie/static/js/2.efd291a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89d6122c38a467f143e",
+    "url": "/gibmovie/static/js/main.c9ba31a4.chunk.js"
+  },
+  {
+    "revision": "30092c3e89509fa84c72",
+    "url": "/gibmovie/static/js/runtime-main.ad2568b6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gibmovie/precache-manifest.79c7be4a5e0800a0a0651fb9753c4b65.js"
+  "/gibmovie/precache-manifest.bf2fb67363de80f90c6c9a73d1c1149a.js"
 );
 
 self.addEventListener('message', (event) => {
