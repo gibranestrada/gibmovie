d19bac846aa9f05df5c63365182a0bd313e5709d
Gibran Estrada <gibran9@gmail.com>
2020-04-07 13:17:59 -0400

    minor change

--- .netlify/functions/apiRequest.js
+++ .netlify/functions/apiRequest.js
@@ -1,11 +1,11 @@
 const Axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
-  const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
+  const pass = (body) => {callback(null, {statusCode: 200, body: body})}
   const API_KEY=process.env.REACT_APP_API_KEY
     const MOVIE_API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
     await Axios(MOVIE_API_URL)
-        .then(res=>{pass(res)})
+        .then(res=>{console.log(res);pass(res)})
 //    let data = await response.json()
 //    await pass(data)
 //  } catch(err) {
