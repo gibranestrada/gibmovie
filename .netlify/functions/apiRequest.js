e8bd0595d5b2d57e455a0a8762591c25d615ab10
Gibran Estrada <gibran9@gmail.com>
2020-04-07 13:13:42 -0400

    changed axios import

--- .netlify/functions/apiRequest.js
+++ .netlify/functions/apiRequest.js
@@ -1,10 +1,10 @@
-import Axios from "axios";
+const Axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
   const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
   const API_KEY=process.env.REACT_APP_API_KEY
     const MOVIE_API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
-    const movies = await Axios(MOVIE_API_URL)
+    await Axios(MOVIE_API_URL)
         .then(res=>{pass(res)})
 //    let data = await response.json()
 //    await pass(data)
