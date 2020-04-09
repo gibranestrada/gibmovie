5214a5f895df0cbff34fefc4e0be406541e4bf37
gibran <gibran@altronix.com>
2020-04-09 10:30:04 -0400

    netlify

--- .netlify/functions/apiRequest.js
+++ .netlify/functions/apiRequest.js
@@ -1,11 +1,12 @@
 const Axios = require('axios');
 require('dotenv').config()
 exports.handler = async (event, context, callback) => {
-  const pass = (body) => {callback(null, {statusCode: 200, body: body})}
+  const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
   const API_KEY=process.env.API_KEY
     const MOVIE_API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
     await Axios(MOVIE_API_URL)
         .then(res=>{pass(res.data)})
+        .catch(e=>{console.log(e)})
 //    let data = await response.json()
 //    await pass(data)
 //  } catch(err) {
