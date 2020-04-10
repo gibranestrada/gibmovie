841eadd20312f4945e9d1c7a732e5dd4c3df0e4d
Gibran Estrada <gibran9@gmail.com>
2020-04-10 14:54:04 -0400

    changed app request to post

--- .netlify/functions/apiRequest.js
+++ .netlify/functions/apiRequest.js
@@ -2,8 +2,12 @@ const Axios = require('axios');
 require('dotenv').config()
 exports.handler = async (event, context, callback) => {
   const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
   const API_KEY=process.env.API_KEY
-    const MOVIE_API_URL = `${event.queryStringParameters.first}${API_KEY}${event.queryStringParameters.second}`;
+  const params = queryString.parse(event.body);
+  const MOVIE_API_URL = `${params.first}${API_KEY}${params.second}`;
     await Axios(MOVIE_API_URL)
         .then(res=>{pass(res.data)})
         .catch(e=>{console.log(e)})
